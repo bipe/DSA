c0c6b975b3c4e4e93c37275daad8eda238edeebf
Luis Felipe Marconi <1bipe@pm.me>
2024-01-24 20:56:18 -0300

    add queue in TS

---
+++ data-structures/queue/tsQueue.ts
@@ -0,0 +1,53 @@
+type Node<T> = {
+    value: T,
+    next?: Node<T>,
+}
+
+export default class Queue<T> {
+    
+    public length: number;
+    private head?: Node<T>
+    private tail?: Node<T>
+
+    constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
+    }
+
+    enqueue(item: T): void {
+        const newNode: Node<T> = { value: item }
+
+        this.length++;
+
+        if (!this.tail) {
+            this.tail = this.head = newNode;
+            return;
+        }
+
+        this.tail.next = newNode;
+        this.tail = newNode;
+    }
+
+    deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
+
+        this.length--;
+
+        const aux = this.head;
+        this.head = this.head.next;
+        // JS GC takes care of freeing up the memory from old node
+        aux.next = undefined;
+
+        if (this.length === 0){
+            this.tail = undefined;
+        }
+
+        return aux.value;
+    }
+
+    peek(): T | undefined {
+        return this.head?.value;
+    }
+}
